b82ce6089203b8721235bbfc22aeb2f5b88a1660
Abdirisak Ahmed <khubaro2017@gmail.com>
2022-10-21 19:45:18 +0300

    CSS file Was added

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Training</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="./test/index.css">
 </head>
 <body>
 <div class="container-xxl">
     <div class="row justify-content-center">
-        <div class="col-sm-8 border border-2 rounded mt-5">
+        <div class="col-sm-8 border border-2 rounded mt-5 myRow">
             <h1 class="text-center">User Registeration</h1>
             <p class="text-center">Register To Access Our Dashboard</p>
             <form action="">
