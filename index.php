bc685829a3f87efebd8619e97debad4855293505
Abdirisak Ahmed <khubaro2017@gmail.com>
2022-10-21 19:36:25 +0300

    One FIle added

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Training</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-xxl">
+    <div class="row justify-content-center">
+        <div class="col-sm-8 border border-2 rounded mt-5">
+            <h1 class="text-center">User Registeration</h1>
+            <p class="text-center">Register To Access Our Dashboard</p>
+            <form action="">
+                <label for="">Username</label>
+                <input type="text" name="" id="" class="form-control">
+                <label for="">Name</label>
+                <input type="text" name="" id="" class="form-control">
+                <label for="">Password</label>
+                <input type="text" name="" id="" class="form-control mb-5">
+                <button class="btn btn-success mb-5">Regiter</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
